163b2dee03cea917c156c00103a0a335ecf18a46
JewishCat <chudikov3@gmail.com>
2023-02-25 13:59:42 +0300

    Worked

---
+++ yatube/posts/models.py
@@ -0,0 +1,49 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+    )
+    description = models.TextField(
+        max_length=400,
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField(
+        max_length=400,
+        help_text='Введите текст поста',
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        help_text='Выберите группу'
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text
